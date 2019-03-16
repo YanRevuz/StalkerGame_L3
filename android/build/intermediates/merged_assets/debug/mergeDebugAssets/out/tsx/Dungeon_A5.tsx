c414512ccc101fd2af4dd720dbc1f728f2eb5fa1
Yan Revuz <yan.revuz@univ-tlse3.fr>
2019-03-16 19:34:24 +0100

    add project

---
+++ android/build/intermediates/merged_assets/debug/mergeDebugAssets/out/tsx/Dungeon_A5.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Dungeon_A5" tilewidth="32" tileheight="32" tilecount="128" columns="8">
+ <image source="Dungeon_A5.png" width="256" height="512"/>
+</tileset>
---
+++ android/build/intermediates/merged_assets/debug/mergeDebugAssets/out/tsx/Dungeon_C.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Dungeon_C" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="Dungeon_C.png" width="512" height="512"/>
+</tileset>
